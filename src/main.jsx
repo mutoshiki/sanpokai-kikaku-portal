ae4760d1228a41b4394d98c11d7da72dead3d8b2
mutoshiki <xh26zjnd8y@privaterelay.appleid.com>
2026-08-23 22:57:09 +0900

    Mount dark mode toggle

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import ThemeToggle from './ThemeToggle.jsx';
 import './styles.scss';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <ThemeToggle />
   </React.StrictMode>,
 );
