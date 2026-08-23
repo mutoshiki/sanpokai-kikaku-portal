467bfd6df9c8108e4d351618c61a6977cc6db304
mutoshiki <xh26zjnd8y@privaterelay.appleid.com>
2026-08-23 23:59:45 +0900

    Let Carbon Theme own the root layer

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Layer } from '@carbon/react';
 import App from './App.jsx';
 import { ThemePreferenceProvider } from './ThemeToggle.jsx';
 import './styles.scss';
@@ -8,9 +7,7 @@ import './styles.scss';
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemePreferenceProvider>
-      <Layer level={0}>
-        <App />
-      </Layer>
+      <App />
     </ThemePreferenceProvider>
   </React.StrictMode>,
 );
